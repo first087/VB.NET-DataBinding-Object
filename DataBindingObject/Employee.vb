67d745d03e4aa480f2b326bde6ee253c4ec46b2a
Artit Kiuwilai <first087@gmail.com>
2015-08-03 11:56:08 +0700

    Add Model

---
+++ DataBindingObject/Employee.vb
@@ -0,0 +1,45 @@
+﻿Public Class Employee
+
+    Private _name As String
+    Private _age As Byte
+    Private _gender As GenderEnum
+
+    Public Enum GenderEnum As Byte
+        Unknown
+        Male
+        Female
+    End Enum
+
+    Public Sub New(ByVal name As String, ByVal age As Byte, ByVal gender As GenderEnum)
+        _name = name
+        _age = age
+        _gender = gender
+    End Sub
+
+    Public Property Name() As String
+        Get
+            Return _name
+        End Get
+        Set(ByVal value As String)
+            _name = value
+        End Set
+    End Property
+
+    Public Property Age() As Byte
+        Get
+            Return _age
+        End Get
+        Set(ByVal value As Byte)
+            _age = value
+        End Set
+    End Property
+
+    Public Property Gender() As GenderEnum
+        Get
+            Return _gender
+        End Get
+        Set(ByVal value As GenderEnum)
+            _gender = value
+        End Set
+    End Property
+End Class
